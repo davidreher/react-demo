201730a0a60fcec198ff2ac13589b0fe8e9d1fac
David Reher <david.reher@gmx.de>
2015-10-08 14:55:02 +0200

    Step III

--- js/comments.jsx
+++ js/comments.jsx
@@ -11,21 +11,56 @@ var CommentBox = React.createClass({
 });
 
 var CommentList = React.createClass({
-    render: function() {
+    render: function () {
         return (
             <div className="commentList">
-                Hello, world! I am a CommentList.
+                <Comment author="Pete Hunt">This is one comment</Comment>
+                <Comment author="Jordan Walke">This is another comment</Comment>
+            </div>
+        );
+    }
+});
+
+var Comment = React.createClass({
+    propTypes: {
+        author: React.PropTypes.string.isRequired
+    },
+    render: function () {
+        return (
+            <div className="comment">
+                <h2 className="commentAuthor">
+                    {this.props.author}
+                </h2>
+                <span>{this.props.children}</span>
             </div>
         );
     }
 });
 
 var CommentForm = React.createClass({
-    render: function() {
+    getInitialState: function () {
+        return {
+            name: null,
+            text: null
+        };
+    },
+
+    onNameChanged: function (e) {
+        this.setState({name: e.target.value});
+    },
+
+    onTextChanged: function (e) {
+        this.setState({text: e.target.value});
+    },
+
+    render: function () {
         return (
-            <div className="commentForm">
-                Hello, world! I am a CommentForm.
-            </div>
+            <form className="commentForm">
+                <input type="text" placeholder="Your name" onChange={this.onNameChanged} value={this.state.name}/>
+                <input type="text" placeholder="Say something..." onChange={this.onTextChanged}
+                       value={this.state.text}/>
+                <input type="submit"/>
+            </form>
         );
     }
 });
